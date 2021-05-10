673e4ddc396c78ea6697c3f3a5d97027b0555b98
sokolovgm <sokolov.gm@phyctech.edu>
2021-05-10 15:17:48 +0300

    Start scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class MenuScript : MonoBehaviour
+    {
+        [SerializeField] private SceneAsset m_Scene;
+        public void StartGame()
+        {
+            SceneManager.LoadScene(m_Scene.name);
+        }
+    }
+}
