6eb42d3463fa150d055f443a06153ec0bda15d59
sokolovgm <sokolov.gm@phyctech.edu>
2021-05-10 16:44:32 +0300

    Прототипы функций

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,7 +1,28 @@
 ﻿namespace Assets.Scripts
 {
-    public class Game
+    namespace Assets.Scripts
     {
-        
+        public static class Game
+        {
+            public static void start_attempt()
+            {
+            
+            }
+
+            public static void get_coin()
+            {
+                
+            }
+
+            public static void finish()
+            {
+                
+            }
+
+            public static void die()
+            {
+                
+            }
+        }
     }
 }
