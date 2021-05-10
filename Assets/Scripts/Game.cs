05f788d2f02c09e465cf6b2e74672f50f4de71df
sokolovgm <sokolov.gm@phyctech.edu>
2021-05-10 17:12:32 +0300

    Взятие денег

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -6,14 +7,22 @@ namespace Assets.Scripts
     {
         public static class Game
         {
+            public static event Action<int> GetMoney;
+            private static int m_Money;
+            public static int Money => m_Money;
+            //public static GameObject Player;
+            
             public static void start_attempt()
             {
-            
+                m_Money = 0;
+                GetMoney?.Invoke(m_Money);
             }
 
             public static void get_coin()
             {
                 Debug.Log("Coin!");
+                ++m_Money;
+                GetMoney?.Invoke(m_Money);
             }
 
             public static void finish()
