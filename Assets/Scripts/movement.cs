38844b9252a979eb4dd33fe265fe9a1cf8479b8a
sokolovgm <sokolov.gm@phyctech.edu>
2021-05-10 16:05:58 +0300

    Плавный поворот

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -15,9 +15,13 @@ public class movement : MonoBehaviour
     private Vector3 m_RightDirection = new Vector3(0, sq_vect, -sq_vect);
     private Vector3 m_LeftDirection = new Vector3(0, -sq_vect, sq_vect);
     private Vector3 m_DownDirection = new Vector3(1, 0, 0);
-    [SerializeField] private float m_RotationAngle;
+    [SerializeField] private float m_MaxRotationAngle;
+    [SerializeField] private float m_RotationSpeed;
     [SerializeField] private Vector3 m_StartPosition;
     [SerializeField] private Vector3 m_BaseRotation;
+
+    //private Quaternion m_CurrentRotation;
+    private Quaternion m_TargetRotation;
 
     void Awake()
     {
@@ -26,34 +30,38 @@ public class movement : MonoBehaviour
         m_DownPressed = Input.GetKeyDown(KeyCode.DownArrow);
         m_LeftPressed = Input.GetKeyDown(KeyCode.LeftArrow);
         m_RightPressed = Input.GetKeyDown(KeyCode.RightArrow);
-        updateRotation();
-        m_SpeedVector = transform.forward * m_Speed;
+        //m_CurrentRotation = Quaternion.Euler(m_BaseRotation);
+        updateTargetRotation();
+        //m_SpeedVector = transform.forward * m_Speed;
         Debug.Log(transform.forward);
         Debug.Log(transform.up);
         Debug.Log(transform.right);
         
     }
  
-    private void updateRotation()
+    private void updateTargetRotation()
     {
         Vector3 rotation = m_BaseRotation;
         if (m_DownPressed)
         {
-            rotation += m_DownDirection * m_RotationAngle;
+            rotation += m_DownDirection * m_MaxRotationAngle;
         }
         if (m_LeftPressed)
         {
-            rotation += m_LeftDirection * m_RotationAngle;
+            rotation += m_LeftDirection * m_MaxRotationAngle;
         }
         if (m_RightPressed)
         {
-            rotation += m_RightDirection * m_RotationAngle;
+            rotation += m_RightDirection * m_MaxRotationAngle;
         }
         if (m_UpPressed)
         {
-            rotation += m_UpDirection * m_RotationAngle;
+            rotation += m_UpDirection * m_MaxRotationAngle;
         }
-        transform.rotation = Quaternion.Euler(rotation);
+        m_TargetRotation = Quaternion.Euler(rotation);
+        //Debug.Log(m_TargetRotation);
+        //Debug.Log(transform.position);
+        //transform.rotation = Quaternion.Euler(rotation);
     }
     
     private bool process_key(KeyCode code, ref bool flag)
@@ -81,9 +89,8 @@ public class movement : MonoBehaviour
         changed |= process_key(KeyCode.RightArrow, ref m_RightPressed);
         if (changed)
         {
-            Debug.Log("Change");
-            updateRotation();
-            m_SpeedVector = transform.forward * m_Speed;
+            //Debug.Log("Change");
+            updateTargetRotation();
         }
     }
 
@@ -99,6 +106,8 @@ public class movement : MonoBehaviour
     void Update()
     {
         process_keys();
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, m_TargetRotation, m_RotationSpeed * Time.deltaTime);
+        m_SpeedVector = transform.forward * m_Speed;
         if (transform.position.z >= target_z)
         {
             return;
