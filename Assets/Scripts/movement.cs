6078dae211f0a6f52c0690f5683375f536a892bb
sokolovgm <sokolov.gm@phyctech.edu>
2021-05-10 14:19:49 +0300

    Fix movement script

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    //[SerializeField]
-    private Vector3 m_Speed;
+    [SerializeField] private float m_Speed;
+    private Vector3 m_SpeedVector;
     [SerializeField] private float target_x;
     // Start is called before the first frame update
     private bool m_UpPressed, m_DownPressed, m_LeftPressed, m_RightPressed;
-    private Vector3 m_UpDirection = new Vector3(0, 0, 1);
+    private Vector3 m_UpDirection = new Vector3(1, 0, 0);
     private Vector3 m_RightDirection = new Vector3(0, 1, 0);
     private Vector3 m_LeftDirection = new Vector3(0, -1, 0);
-    private Vector3 m_DownDirection = new Vector3(0, 0, -1);
+    private Vector3 m_DownDirection = new Vector3(-1, 0, 0);
     [SerializeField] private float m_RotationAngle;
     [SerializeField] private Vector3 m_StartPosition;
     [SerializeField] private Vector3 m_BaseRotation;
@@ -26,36 +26,13 @@ public class movement : MonoBehaviour
         m_LeftPressed = Input.GetKeyDown(KeyCode.LeftArrow);
         m_RightPressed = Input.GetKeyDown(KeyCode.RightArrow);
         updateRotation();
-        m_Speed = -transform.up;
+        m_SpeedVector = -transform.forward * m_Speed;
+        Debug.Log(transform.forward);
+        Debug.Log(transform.up);
+        Debug.Log(transform.right);
         
     }
-    void Start()
-    {
-        //m_Speed = new Vector3(1f, 0f, 0f);
-        m_UpPressed = Input.GetKeyDown(KeyCode.UpArrow);
-        m_DownPressed = Input.GetKeyDown(KeyCode.DownArrow);
-        m_LeftPressed = Input.GetKeyDown(KeyCode.LeftArrow);
-        m_RightPressed = Input.GetKeyDown(KeyCode.RightArrow);
-        if (m_UpPressed)
-        {
-            Debug.Log("Up at start");
-        }
-        if (m_DownPressed)
-        {
-            Debug.Log("Down at start");
-        }
-        if (m_LeftPressed)
-        {
-            Debug.Log("Left at start");
-        }
-        if (m_RightPressed)
-        {
-            Debug.Log("Right at start");
-        }
-        updateRotation();
-        m_Speed = -transform.up;
-    }
-
+ 
     private void updateRotation()
     {
         Vector3 rotation = m_BaseRotation;
@@ -76,21 +53,6 @@ public class movement : MonoBehaviour
             rotation += m_UpDirection * m_RotationAngle;
         }
         transform.rotation = Quaternion.Euler(rotation);
-    }
-
-    private void debug_process_key(KeyCode code, ref bool flag, String name)
-    {
-        if (Input.GetKeyDown(code))
-        {
-            flag = true;
-            Debug.Log(name + " is down");
-        }
-
-        if (Input.GetKeyUp(code))
-        {
-            flag = false;
-            Debug.Log(name + " is up");
-        }
     }
     
     private bool process_key(KeyCode code, ref bool flag)
@@ -120,7 +82,7 @@ public class movement : MonoBehaviour
         {
             Debug.Log("Change");
             updateRotation();
-            m_Speed = -transform.up;
+            m_SpeedVector = -transform.forward * m_Speed;
         }
     }
 
@@ -136,19 +98,14 @@ public class movement : MonoBehaviour
     void Update()
     {
         process_keys();
-        if (transform.position.x > target_x)
+        if (transform.position.x < target_x)
         {
             return;
         }
-        transform.position = transform.position + m_Speed * Time.deltaTime;
+        transform.position = transform.position + m_SpeedVector * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             debug_get_pressed();
         }
-        debug_process_key(KeyCode.UpArrow, ref m_UpPressed, "Up ");
-        debug_process_key(KeyCode.DownArrow, ref m_DownPressed, "Down ");
-        debug_process_key(KeyCode.LeftArrow, ref m_LeftPressed, "Left ");
-        debug_process_key(KeyCode.RightArrow, ref m_RightPressed, "Right ");
-
     }
 }
