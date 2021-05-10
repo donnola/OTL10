ced99bcf6cb1af417f9c9ac0a195720615fe2fbb
donnola <gabbasova.aa@phystech.edu>
2021-05-10 13:13:14 +0300

    aircraft

---
+++ Assets/Scripts/MovementCamera.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCamera : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    [SerializeField] 
+    private float m_distant = 5.0f;
+    [SerializeField]
+    private Transform m_target;
+
+    private void Awake()
+    {
+        if (m_target != null) {
+            transform.position = m_target.position - Vector3.forward * m_distant;
+            transform.LookAt(m_target.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() 
+    { 
+        if (m_target != null) {
+            transform.position = m_target.position - Vector3.forward * m_distant;
+            transform.LookAt(m_target.position);
+        }
+    }
+}
