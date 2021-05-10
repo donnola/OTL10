c386cb869b8a79f1cd2149f33871a2604348796a
donnola <gabbasova.aa@phystech.edu>
2021-05-10 16:58:41 +0300

    .

---
+++ Assets/Scripts/Сollision.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Assets.Scripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Сollision: MonoBehaviour
+    {
+        void OnTriggerEnter (Collider other)
+        {
+            if (other.CompareTag("Wall") || other.CompareTag("Ring"))
+            {
+                Game.die();
+            }
+        }
+    }
+}
